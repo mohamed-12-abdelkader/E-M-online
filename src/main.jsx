ec61469fa31d7cbbd8857d839f614653a76f7e48
unknown <mharedy1212@gmail.com>
2024-07-07 02:04:47 -0700

    Prevent the user from taking a screenshot

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
@@ -10,15 +10,38 @@ import "react-toastify/dist/ReactToastify.css";
 import WhatsButton from "./components/whatsButton/WhatsButton.jsx";
 import SidebarWithHeader from "./components/navbar/Navbar.jsx";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <ChakraProvider>
-      <Router>
-        <SidebarWithHeader />
-        <App />
-        <WhatsButton />
-        <Footer />
-      </Router>
-    </ChakraProvider>
-  </React.StrictMode>
-);
+const Root = () => {
+  useEffect(() => {
+    const overlay = document.getElementById("overlay");
+
+    const onVisibilityChange = () => {
+      if (document.visibilityState === "hidden") {
+        overlay.style.display = "block";
+      } else {
+        overlay.style.display = "none";
+      }
+    };
+
+    document.addEventListener("visibilitychange", onVisibilityChange);
+
+    return () => {
+      document.removeEventListener("visibilitychange", onVisibilityChange);
+    };
+  }, []);
+
+  return (
+    <React.StrictMode>
+      <div id="overlay" className="overlay"></div>
+      <ChakraProvider>
+        <Router>
+          <SidebarWithHeader />
+          <App />
+          <WhatsButton />
+          <Footer />
+        </Router>
+      </ChakraProvider>
+    </React.StrictMode>
+  );
+};
+
+ReactDOM.createRoot(document.getElementById("root")).render(<Root />);

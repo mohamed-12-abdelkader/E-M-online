fa4793741bf3415d6b5a0ec14e665db0304123f6
unknown <mharedy1212@gmail.com>
2024-08-06 20:35:25 -0700

    save scren

--- src/components/teacher/AllTeacherLogin.jsx
+++ src/components/teacher/AllTeacherLogin.jsx
@@ -1,88 +1,183 @@
+import { useState } from "react";
 import { PiChalkboardTeacherBold } from "react-icons/pi";
-import { Card, CardBody, Skeleton, Stack } from "@chakra-ui/react";
+import {
+  Box,
+  Heading,
+  Spinner,
+  Button,
+  FormControl,
+  Select,
+  Image,
+  Flex,
+  Text,
+  VStack,
+} from "@chakra-ui/react";
 import { MdCancelPresentation } from "react-icons/md";
 import { Link } from "react-router-dom";
-import { Zoom } from "react-awesome-reveal";
 import { FaVideo } from "react-icons/fa";
 import GitTeacherByToken from "../../Hooks/student/GitTeacherByToken";
+
 const AllTeacherLogin = () => {
   const [loading, teachers] = GitTeacherByToken();
+  const [selectedStream, setSelectedStream] = useState("");
+  const [selectedSubject, setSelectedSubject] = useState("");
+
+  const handleStreamChange = (e) => {
+    setSelectedStream(e.target.value);
+  };
+
+  const handleSubjectChange = (e) => {
+    setSelectedSubject(e.target.value);
+  };
+
+  const filteredTeachers = Array.isArray(teachers)
+    ? teachers.filter((teacher) => {
+        let isValid = true;
+
+        if (selectedStream === "علمي") {
+          isValid = ![
+            "علم نفس واجتماع  ",
+            " فلسفة ومنطق   ",
+            "تاريخ",
+            "جغرافيا",
+          ].includes(teacher.subject);
+        } else if (selectedStream === "ادبي") {
+          isValid = ![
+            "فيزياء",
+            "كيمياء",
+            "احياء",
+            "جيولوجيا",
+            "رياضة",
+          ].includes(teacher.subject);
+        }
+
+        if (selectedSubject) {
+          isValid =
+            isValid && teacher.subject.trim() === selectedSubject.trim();
+        }
+
+        return isValid;
+      })
+    : [];
+
   return (
-    <div className=" p-5 w-[100%] my-[50px]">
-      <div className="p-5">
-        <h1
-          className="fonts font-bold text-3xl flex text-blue-500 my-3"
-          style={{ fontWeight: "bold", fontSize: "50px" }}
-        >
-          <PiChalkboardTeacherBold className="m-1 mx-2 text-red-500" />
-          المدرسين
-        </h1>
-      </div>
-      <div>
+    <Box w="100%" my="5" px={{ base: "3", md: "5" }}>
+      <Heading
+        as="h1"
+        fontSize="50px"
+        color="blue.500"
+        display="flex"
+        alignItems="center"
+        mb="2"
+      >
+        <PiChalkboardTeacherBold className=" mx-2 text-red-500" />
+        المدرسين
+      </Heading>
+      <Flex justify="center" px="3">
+        <FormControl w="200px" m="2">
+          <Select placeholder="اختر الشعبة" onChange={handleStreamChange}>
+            <option value="علمي">علمي</option>
+            <option value="ادبي">ادبي</option>
+          </Select>
+        </FormControl>
+        <FormControl w="200px" m="2">
+          <Select placeholder="اختر المادة" onChange={handleSubjectChange}>
+            <option value="لغة عربية ">لغة عربية</option>
+            <option value="English">لغة انجليزية</option>
+            <option value="French">لغة فرنسية</option>
+            <option value="فيزياء ">فيزياء</option>
+            <option value="كيمياء ">كيمياء</option>
+            <option value="احياء ">احياء</option>
+            <option value="جيولوجيا ">جيولوجيا</option>
+            <option value="تاريخ">تاريخ</option>
+            <option value="جغرفيا ">جغرافيا</option>
+            <option value=" فلسفة ومنطق ">فلسفة</option>
+            <option value="علم نفس واجتماع ">علم نفس</option>
+            <option value="رياضيات ">رياضيات</option>
+          </Select>
+        </FormControl>
+      </Flex>
+      <Box>
         {loading ? (
-          <Stack className="w-[90%] m-auto my-5">
-            <Skeleton height="20px" className="mt-5" />
-            <Skeleton height="20px" />
-            <Skeleton height="20px" />
-            <Skeleton height="20px" />
-            <Skeleton height="20px" />
-            <Skeleton height="20px" />
-            <Skeleton height="20px" />
-          </Stack>
-        ) : teachers && teachers.length > 0 ? (
-          <div
-            className="w-[95%] m-auto teacher-card-content   flex md:flex  flex-wrap"
-            style={{ borderRadius: "20px" }}
+          <Flex w="90%" m="auto" my="5" justify="center" flexWrap="wrap">
+            <Spinner size="xl" color="primary" m="2" />
+            <Spinner size="xl" color="primary" m="2" />
+            <Spinner size="xl" color="primary" m="2" />
+            <Spinner size="xl" color="primary" m="2" />
+            <Spinner size="xl" color="primary" m="2" />
+            <Spinner size="xl" color="primary" m="2" />
+            <Spinner size="xl" color="primary" m="2" />
+          </Flex>
+        ) : filteredTeachers.length > 0 ? (
+          <Flex
+            w="95%"
+            m="auto"
+            flexWrap="wrap"
+            justify={{ base: "center", md: "start" }}
+            bg="white"
+            p="5"
           >
-            <div className="flex justify-center card-content p-3 flex-wrap md:justify-start   ">
-              {teachers.map((teacher) => (
-                <Card
-                  key={teacher.id}
-                  className=" w-[300px] my-3  md:mx-7 w-[330px] m-2 "
-                  style={{ border: "1px solid #ccc" }}
-                >
-                  <Link to={`teacher/${teacher.id}`}>
-                    <CardBody>
-                      <img
-                        src={teacher.image}
-                        className="h-[210px] w-[100%] border "
-                        style={{ borderRadius: "10px" }}
-                        alt="Course"
-                      />
-                      <div className="flex justify-between">
-                        <h1 color="blue.600" className="font-bold mt-2">
-                          {teacher.name}
-                        </h1>
-                        <h1 color="blue.600" className="font-bold mt-2">
-                          {teacher.subject}
-                        </h1>
-                      </div>
-                    </CardBody>
-                    <hr className="w-[90%] m-auto" />
-                    <div className="px-5 my-3">
-                      <h1 className="font-bold flex">
-                        <FaVideo className="m-2 text-red-500" />
-                        {teacher.description}
-                      </h1>
-                    </div>
-                  </Link>
-                </Card>
-              ))}
-            </div>
-          </div>
+            {filteredTeachers.map((teacher) => (
+              <Box
+                key={teacher.id}
+                w="300px"
+                my="3"
+                mx={{ base: "2", md: "10" }}
+                borderWidth="1px"
+                borderRadius="lg"
+                overflow="hidden"
+                boxShadow="md"
+              >
+                <Link to={`teacher/${teacher.id}`}>
+                  <Box>
+                    <Image
+                      src={teacher.image}
+                      h="220px"
+                      w="100%"
+                      borderRadius="10px"
+                      alt="Course"
+                    />
+                    <Flex justify="space-between" py="2">
+                      <Text fontWeight="bold" mt="2">
+                        {teacher.name}
+                      </Text>
+                      <Text fontWeight="bold" mt="2">
+                        {teacher.subject}
+                      </Text>
+                    </Flex>
+                  </Box>
+                  <hr className="w-[90%] m-auto" />
+                  <Box px="2" my="3">
+                    <Text fontWeight="bold" display="flex" alignItems="center">
+                      <FaVideo className="m-1 text-red-500" />
+                      {teacher.description}
+                    </Text>
+                  </Box>
+                </Link>
+              </Box>
+            ))}
+          </Flex>
         ) : (
-          <div
-            className="text-center py-5 bg-white h-[200px] flex justify-center items-center"
-            style={{ borderRadius: "20px" }}
+          <Flex
+            justify="center"
+            alignItems="center"
+            bg="white"
+            h="200px"
+            borderRadius="20px"
           >
-            <h1 className="font-bold flex justify-center text-black">
+            <Text
+              fontWeight="bold"
+              display="flex"
+              alignItems="center"
+              color="black"
+            >
               <MdCancelPresentation className="m-1 text-red-500" />
               لا يوجد مدرسين الان
-            </h1>
-          </div>
+            </Text>
+          </Flex>
         )}
-      </div>
-    </div>
+      </Box>
+    </Box>
   );
 };
 
--- src/pages/leacter/Vedio.jsx
+++ src/pages/leacter/Vedio.jsx
@@ -1,16 +1,27 @@
+import { useState, useEffect, useRef } from "react";
 import { Skeleton, Stack } from "@chakra-ui/react";
-
 import { useParams } from "react-router-dom";
 import GitVedio from "../../Hooks/student/GitVedio";
-
 import "react-html5video/dist/styles.css";
 import GitVediot from "../../Hooks/teacher/GitVediot";
 import ScrollToTop from "../../components/scollToTop/ScrollToTop";
+
 const Vedio = () => {
   const { videoId } = useParams();
-
   const [vedioLoading, vdiourl] = GitVedio({ id: videoId });
   const [vedioLoadingt, vdiourlt] = GitVediot({ id: videoId });
+  const [isRecording, setIsRecording] = useState(false);
+  const inputRef = useRef(null);
+
+  useEffect(() => {
+    if (isRecording && inputRef.current) {
+      inputRef.current.focus();
+    }
+  }, [isRecording]);
+
+  useEffect(() => {
+    setIsRecording(true);
+  }, []);
 
   if (vedioLoading || vedioLoadingt) {
     return (
@@ -27,17 +38,17 @@ const Vedio = () => {
     );
   }
 
-  console.log(vdiourlt.video);
   return (
-    <div className="w-100% mt-[50px] ">
+    <div className={`w-100% mt-[50px] `}>
       <iframe
         src={vdiourl.video || vdiourlt.video}
         loading="lazy"
         className="w-[100%] h-[120vh] m-auto border"
         allow="accelerometer;gyroscope;autoplay;encrypted-media;picture-in-picture;"
-        allowfullscreen="true"
+        allowFullScreen="true"
       ></iframe>
       <ScrollToTop />
+      <input ref={inputRef} className="input-vedio" type="password" />
     </div>
   );
 };
